4972bc8e3df773da01ef7fd2d334868194a0bd5b
Guilherme Zanellato <guizanellato@gmail.com>
2015-09-25 21:05:41 -0300

    Initial Commit

---
+++ GZGitSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GZGitSearch
+//
+//  Created by Guilherme Zanellato on 9/25/15.
+//  Copyright © 2015 Guilherme Zanellato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GZGitSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GZGitSearch
+//
+//  Created by Guilherme Zanellato on 9/25/15.
+//  Copyright © 2015 Guilherme Zanellato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
